23eebb4503a7b7e781a782b5567e75f28c175627
Tom Black <tom.black06007@gmail.com>
2022-01-30 15:21:21 -0700

    First commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Expenses from "./components/Expenses";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's Get Started!</h2>
+      <Expenses></Expenses>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+import './Expenses.css';
+import ExpenseItem from "./ExpenseItems";
+
+function Expenses(props) {
+    const expenses = [
+        {title: 'Insurance', amount: 289.49, date: new Date(2022, 2, 28)},
+        {title: 'Groceries', amount: 150.51, date: new Date(2022, 1, 28)},
+        {title: 'Gift Cards', amount: 100.99, date: new Date(2022, 2, 20)},
+        {title: 'Gym', amount: 25.00, date: new Date(2022, 2, 28)},
+      ]
+    return (
+        <div className="expenses">
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+        </div>
+    );
+}
+
+export default Expenses;
