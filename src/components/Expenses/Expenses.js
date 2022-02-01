1058ad65868c7c6c506e92ca78a3cca05bad89d0
Tom Black <tom.black06007@gmail.com>
2022-01-31 21:37:00 -0700

    Reorganized the code/components.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,6 @@
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItems";
+import Card from '../UI/Card';
 
 function Expenses(props) {
     const expenses = [
@@ -9,12 +10,12 @@ function Expenses(props) {
         {title: 'Gym', amount: 25.00, date: new Date(2022, 2, 28)},
       ]
     return (
-        <div className="expenses">
+        <Card className="expenses">
         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
         <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
         <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-        </div>
+        </Card>
     );
 }
 
